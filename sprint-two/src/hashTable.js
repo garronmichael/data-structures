f5f8e11f0bf016ab6cc61949e8e029a3a36a9db4
garronmichael <garron.michael@gmail.com>
2015-02-06 21:50:01 -0800

    pass all tests but collision, begin refactor for collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,22 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage[i]) {
+    this._storage[i].push([k, v]);
+  } else {
+    this._storage[i] = [];
+    this._storage.push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage[i];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = null;
 };
 
 

9e0b458cafa7ef1a51db284662796e5ac8f62fc2
garronmichael <garron.michael@gmail.com>
2015-02-06 21:08:06 -0800

    completed binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,57 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var fn = function(node) {
+    if(value > node.value) {
+      if(node.right) {
+        fn(node.right);
+      } else {
+        node.right = BinarySearchTree(value);
+      }
+    } else if(value < node.value) {
+      if(node.left) {
+        fn(node.left);
+      } else {
+        node.left = BinarySearchTree(value);
+      }
+    }
+  };
+
+  fn(this);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  var apply = function(node) {
+    cb(node.value);
+    if(node.left) {
+      apply(node.left);
+    }
+    if(node.right) {
+      apply(node.right);
+    }
+  }
+  apply(this);
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  var found = false;
+  var find = function(node) {
+    if(node.value === value) {
+      found = true;
+    } else if(node.left && node.value > value) {
+      return find(node.left);
+    } else if(node.right && node.value < value) {
+      return find(node.right);
+    }
+  }
+  find(this);
+  return found;
 };
 
 
